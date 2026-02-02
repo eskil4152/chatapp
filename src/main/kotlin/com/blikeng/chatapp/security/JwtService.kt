5d90a2d11f4894eb9c843ee57a2a275fd1a6f353
eskil <eskil@blikeng.com>
2026-02-02 20:06:34 +0100

    Map dotenv to system

--- src/main/kotlin/com/blikeng/chatapp/security/JwtService.kt
+++ src/main/kotlin/com/blikeng/chatapp/security/JwtService.kt
@@ -11,7 +11,7 @@ import javax.crypto.SecretKey
 
 @Service
 class JwtService {
-    val secret = System.getProperty("JWT_SECRET") ?: "secret"
+    val secret = System.getProperty("JWT_SECRET") ?: System.getenv("JWT_SECRET") ?: "secret"
     private val key: SecretKey = Keys.hmacShaKeyFor(secret.encodeToByteArray())
 
     fun generateToken(user: UserEntity): String {
