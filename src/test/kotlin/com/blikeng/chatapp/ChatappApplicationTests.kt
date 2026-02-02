774a278c54aab3505c507a0d0b27c270ba53c37b
eskil <eskil@blikeng.com>
2026-02-02 22:21:01 +0100

    Literally put resources in wrong dir

--- src/test/kotlin/com/blikeng/chatapp/ChatappApplicationTests.kt
+++ src/test/kotlin/com/blikeng/chatapp/ChatappApplicationTests.kt
@@ -2,16 +2,12 @@ package com.blikeng.chatapp
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.test.context.ActiveProfiles
 
-@EnableJpaRepositories
-@SpringBootTest
 @ActiveProfiles("test")
+@SpringBootTest
 class ChatappApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
-
 }
