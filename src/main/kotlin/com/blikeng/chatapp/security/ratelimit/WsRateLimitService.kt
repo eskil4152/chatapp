073679091b3f6d4c74279fff76f6d28138c7c470
eskil <eskil@blikeng.com>
2026-04-14 14:03:21 +0200

    Use caffeine cache to fix rate-limit buckets growing indefinitely.

--- src/main/kotlin/com/blikeng/chatapp/security/ratelimit/WsRateLimitService.kt
+++ src/main/kotlin/com/blikeng/chatapp/security/ratelimit/WsRateLimitService.kt
@@ -1,13 +1,13 @@
 package com.blikeng.chatapp.security.ratelimit
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.Bucket
-import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.binder.cache.CaffeineStatsCounter
 import org.springframework.stereotype.Service
 import java.time.Duration
 import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 
 // ==========================
 // File for WebSocket message rate limiting. Injected in WebSocketHandler.
@@ -17,14 +17,13 @@ import java.util.concurrent.ConcurrentHashMap
 class WsRateLimitService(
     meterRegistry: MeterRegistry
 ) {
-    private val buckets = ConcurrentHashMap<UUID, Bucket>()
-
-    init {
-        meterRegistry.gauge("ws.rate.limit.buckets", buckets) { it.size.toDouble() }
-    }
+    private val buckets = Caffeine.newBuilder()
+        .expireAfterAccess(Duration.ofMinutes(10))
+        .recordStats { CaffeineStatsCounter(meterRegistry, "ws.rate.limit.buckets") }
+        .build<UUID, Bucket>()
 
     fun tryConsumeMessage(userId: UUID): Boolean {
-        val bucket = buckets.computeIfAbsent(userId) {
+        val bucket = buckets.get(userId) {
             Bucket.builder()
                 .addLimit(
                     Bandwidth.builder()
